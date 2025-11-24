ff2c9a4971c025f44f4f507e91abbf5d3b2a3434
mrhamey <mrhamey9@gmail.com>
2025-11-24 10:46:06 -0500

    initial commit
    initial commit

---
+++ data/publicfacilities.js
@@ -0,0 +1,306 @@
+var json_publicfacilities = 
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.63393, 45.57133] },
+      "properties": {
+        "name": "École Louis-Joseph-Papineau",
+        "longitude": -73.63393,
+        "latitude": 45.57133,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.62987, 45.5701] },
+      "properties": {
+        "name": "École Marie-Rivier",
+        "longitude": -73.62987,
+        "latitude": 45.5701,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.62535, 45.57097] },
+      "properties": {
+        "name": "École Sainte-Lucie",
+        "longitude": -73.62535,
+        "latitude": 45.57097,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.62246, 45.56991] },
+      "properties": {
+        "name": "École Montcalm",
+        "longitude": -73.62246,
+        "latitude": 45.56991,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.61083, 45.56132] },
+      "properties": {
+        "name": "École Saint-Bernardin",
+        "longitude": -73.61083,
+        "latitude": 45.56132,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.61683, 45.55269] },
+      "properties": {
+        "name": "École Saint-Grégoire-le-Grand",
+        "longitude": -73.61683,
+        "latitude": 45.55269,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.61331, 45.57909] },
+      "properties": {
+        "name": "École Saint-Noël-Chabanel",
+        "longitude": -73.61331,
+        "latitude": 45.57909,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.6221, 45.58073] },
+      "properties": {
+        "name": "Centre éducatif communautaire René-Goupil",
+        "longitude": -73.6221,
+        "latitude": 45.58073,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.62257, 45.58335] },
+      "properties": {
+        "name": "École Bienville",
+        "longitude": -73.62257,
+        "latitude": 45.58335,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.59917, 45.5688] },
+      "properties": {
+        "name": "École Léonard-De Vinci",
+        "longitude": -73.59917,
+        "latitude": 45.5688,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.60002, 45.56328] },
+      "properties": {
+        "name": "École Joseph-François-Perrault",
+        "longitude": -73.60002,
+        "latitude": 45.56328,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.60406, 45.55982] },
+      "properties": {
+        "name": "École John F. Kennedy",
+        "longitude": -73.60406,
+        "latitude": 45.55982,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.59826, 45.55603] },
+      "properties": {
+        "name": "École Saint-Mattieu",
+        "longitude": -73.59826,
+        "latitude": 45.55603,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.60267, 45.55033] },
+      "properties": {
+        "name": "École Sainte Barthélemy ",
+        "longitude": -73.60267,
+        "latitude": 45.55033,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.5999167, 45.5598056] },
+      "properties": {
+        "name": "Metro Saint-Michel",
+        "longitude": -73.5999167,
+        "latitude": 45.5598056,
+        "facilitytype": "metro",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.6021389, 45.5537778] },
+      "properties": {
+        "name": "Metro D'Iberville",
+        "longitude": -73.6021389,
+        "latitude": 45.5537778,
+        "facilitytype": "metro",
+        "adaptationmeasures": "metro"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.607426, 45.547885] },
+      "properties": {
+        "name": "Metro Fabre",
+        "longitude": -73.607426,
+        "latitude": 45.547885,
+        "facilitytype": "metro",
+        "adaptationmeasures": "metro"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.601426, 45.561802] },
+      "properties": {
+        "name": "Loisirs communautaires Saint-Michel",
+        "longitude": -73.601426,
+        "latitude": 45.561802,
+        "facilitytype": "recreation",
+        "adaptationmeasures": "recreation"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.639638, 45.563283] },
+      "properties": {
+        "name": "Stade de soccer de Montréal",
+        "longitude": -73.639638,
+        "latitude": 45.563283,
+        "facilitytype": "recreation",
+        "adaptationmeasures": "recreation"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.635266, 45.561356 ] },
+      "properties": {
+        "name": "Le TAZ",
+        "longitude": -73.635266,
+        "latitude": 45.561356 ,
+        "facilitytype": "recreation",
+        "adaptationmeasures": "recreation"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.607269, 45.565512] },
+      "properties": {
+        "name": "Arena Saint-Michel",
+        "longitude": -73.607269,
+        "latitude": 45.565512 ,
+        "facilitytype": "recreation",
+        "adaptationmeasures": "recreation"
+      }
+    },
+{
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.601987, 45.562397] },
+      "properties": {
+        "name": "Bibliothèque de Saint-Michel",
+        "longitude": -73.601987,
+        "latitude": 45.562397 ,
+        "facilitytype": "recreation",
+        "adaptationmeasures": "recreation"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.631863, 45.571729] },
+      "properties": {
+        "name": "Bibliothèque des jeunes de Montréal",
+        "longitude": -73.631863,
+        "latitude": 45.571729 ,
+        "facilitytype": "recreation",
+        "adaptationmeasures": "recreation"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.627925, 45.568460] },
+      "properties": {
+        "name": "École Sophie-Barat",
+        "longitude": -73.627925,
+        "latitude": 45.568460 ,
+        "facilitytype": "school",
+        "adaptationmeasures": "school"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.601882, 45.559768]},
+      "properties": {
+        "name": "CHSLD Joseph-François-Perrault",
+        "longitude": -73.601882,
+        "latitude": 45.568460 ,
+        "facilitytype": "health",
+        "adaptationmeasures": "health"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.610445, 45.562161 ] },
+      "properties": {
+        "name": "CHSLD de Saint-Michel",
+        "longitude": -73.610445,
+        "latitude": 45.562161 ,
+        "facilitytype": "health",
+        "adaptationmeasures": "health"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-73.608301, 45.565406] },
+      "properties": {
+        "name": "CLSC de Saint-Michel",
+        "longitude": -73.608301,
+        "latitude":  45.565406,
+        "facilitytype": "health",
+        "adaptationmeasures": "health"
+      }
+    }
+
+
+
+  ]
+}
