9a08ac77093aee1a60961c0327a0df37f9d298f6
mrhamey <mrhamey9@gmail.com>
2025-11-25 22:46:55 -0500

    updated adaptationmeasureslayer
    updated adaptationmeasureslayer

---
+++ data/adaptationmeasures.js
@@ -0,0 +1,260 @@
+var json_adaptationmeasures = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.607275,
+          45.565522
+        ]
+      },
+      "properties": {
+        "name": "Arena Saint-Michel rainwater recuperation",
+        "implementation": "Implemented",
+        "description": "System that captures rainwater, filters and reuses it for the arena. Implemented to help with sewer overloading during rainstorms."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.62800,
+          45.5699064
+        ]
+      },
+      "properties": {
+        "name": "Increase roof reflectivity",
+        "implementation": "Implemented",
+        "description": "Regulating roof coverings to ensure that they are reflective and raise the local albedo to reduce UHI."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.6294057,
+          45.57159777
+        ]
+      },
+      "properties": {
+        "name": "Increase front yard green space",
+        "implementation": "Implemented",
+        "description": "Change the urban planning\r\nregulations to ensure that front\r\nyards remain green spaces\r\nand prohibit them from being\r\ntransformed into hard surfaces\r\n(ashpalt, concrete, etc.). Permeable surfaces such as vegetation help prevent both water pooling and help reduce heat absorption. "
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.62713336,
+          45.56362271
+        ]
+      },
+      "properties": {
+        "name": "Climate-adapted vegetation",
+        "implementation": "Implemented",
+        "description": "Implemented in an ongoing fashion in Borough greening projects. No further information."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.62433539,
+          45.57030269
+        ]
+      },
+      "properties": {
+        "name": "Increase street sweeping frequency",
+        "implementation": "Implemented",
+        "description": "Sweeping the entire Borough at least once a week in the summer reduces debris buildup in manholes. Helpes to ensure proper water flows during heavy rains"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.63249364,
+          45.55936125
+        ]
+      },
+      "properties": {
+        "name": "Increase the use of continuous tree\r\ntrenches",
+        "implementation": "Implemented",
+        "description": "Implemented during complete street rebuilds. Continuous tree trenches give more room for tree roots to grow, as well as more permeable surface to control storm water runoff and rainwater."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.63490008,
+          45.57124324
+        ]
+      },
+      "properties": {
+        "name": "Increase use of permeable materials in municipal projects",
+        "implementation": "Improved",
+        "description": "Permeable surfaces reduce risks of water pooling and flash flooding during heavy rainfalls. This regulation would be applied during renovations or new municipal projects."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.631298,
+          45.567764
+        ]
+      },
+      "properties": {
+        "name": "Increase treeplanting in the Borough",
+        "implementation": "Implemented",
+        "description": "Borough goal to plant 600 trees per year. Tree planting reduces UHI while improving a number of situations - such as noise and air pollution, and creating shade."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.623609,
+          45.568745
+        ]
+      },
+      "properties": {
+        "name": "Create green and sponge alleys",
+        "implementation": "Proposed",
+        "description": "Borough goal to create 6 \"ruelles vertes\" each year. These ruelles include new vegetation, permeable pavement and less cemented surfaces. This goal could be way more ambitious. "
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.600137,
+          45.561477
+        ]
+      },
+      "properties": {
+        "name": "Create shaded areas",
+        "implementation": "Improved",
+        "description": "Borough plan to create 30 shaded areas per year. Shaded areas are useful for reducing UHI and improving safety during extreme heat events."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.601262,
+          45.561752
+        ]
+      },
+      "properties": {
+        "name": "Increase the availability of air conditioned and heated spaces during weather events",
+        "implementation": "Proposed",
+        "description": "Air conditioned and heated spaces can save lives during extreme heat and storm events, and the city should extend facility hours during heat waves and storms to ensure that people have access throughout the day."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.599587,
+          45.561727
+        ]
+      },
+      "properties": {
+        "name": "Improve natural landscaping practices on city land ",
+        "implementation": "Improved",
+        "description": "Borough plan for one extra site per year of testing. The Saint-Michel area has a disproportionate amount of municipal properties, so city initiatives can use the area for new greening plans easily."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.62743419,
+          45.56993463
+        ]
+      },
+      "properties": {
+        "name": "Begin implementing a separate sewer system",
+        "implementation": "Proposed",
+        "description": "Montr\u00e9al could finally rid itself of sewage overflow during heavy rainfall. It's an incremental, long-term initiative."
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.62702611,
+          45.57310256
+        ]
+      },
+      "properties": {
+        "name": "Sunken driveway ban",
+        "implementation": "Proposed",
+        "description": "Banning this in new development would see the flood risk and damages eventually reduce over the longterm"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.62141961,
+          45.57603224
+        ]
+      },
+      "properties": {
+        "name": "Longterm rehabilitation of Carri\u00e8re Francon",
+        "implementation": "Proposed",
+        "description": "A similar project to Frederich Back could be undertaken to create a new green space with sponge and cooling effects, and would surely lead to reduced vulnerability to heat waves, rain flooding, and other climate hazards in the area between the two spaces. "
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.61884861,
+          45.56073437
+        ]
+      },
+      "properties": {
+        "name": "Implement a pest management plan",
+        "implementation": "Proposed",
+        "description": "A pest management plan is overdue for the borough, seeing as climate-induced pest migration becomes a pressing issue for less-biodiverse urban green spaces"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.61612488,
+          45.55856195
+        ]
+      },
+      "properties": {
+        "name": "Use Frederic-Back Park welcome center for climate adaptation education",
+        "implementation": "Proposed",
+        "description": "The welcome center educates on the history of the park, why not look to the future and encourage citizens to do their part to increase their resiliency to climate change. "
+      }
+    }
+  ]
+}
